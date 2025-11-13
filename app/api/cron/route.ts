c12ad2d15711ff51f9b37dd7dd89baa48faaa5d2
umayerCoding1 <umayerpro175@gmail.com>
2025-11-13 23:04:20 +0600

    redeploy

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 export async function GET() {
 
   fs.writeFileSync('cron.txt', 'cron job success to this day');
+  
   console.log('cron job success to this day')
   return NextResponse.json({ ok: true });
 }
